9a8056e8a5ef7edd0d06b43ca001d3f1ee4010f0
Luis Mayorga <luis.mayorga@agentsmutual.co.uk>
2016-02-13 15:30:47 +0000

    dotemacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,7 @@
 
 (defvar config-dir (expand-file-name "config" user-emacs-directory))
 
-;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
@@ -21,6 +21,7 @@
     align-cljlet
     rainbow-delimiters
     projectile
+    helm-projectile
     magit))
 
 (if (eq system-type 'darwin)
@@ -33,4 +34,16 @@
 ;; Global opts
 (evil-mode 1)
 (menu-bar-mode -1)
+(tool-bar-mode -1)
 (projectile-global-mode)
+(show-paren-mode 1)
+(load-theme 'dracula t)
+
+;; Clojure
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(with-eval-after-load 'clojure-mode
+  (add-hook 'clojure-mode-hook       #'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook       #'rainbow-delimiters-mode)
+  (define-key evil-normal-state-map (kbd "M-.") nil)
+  (global-unset-key (kbd "M-.")))
+
