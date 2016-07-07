675d5e0d2a4d708a7e81a368ac3d574e82acaabb
Luis Mayorga <luis.mayorga@agentsmutual.co.uk>
2016-07-07 17:24:23 +0200

    cljr++

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,5 @@
 (require 'package)
 
-(defvar config-dir (expand-file-name "config" user-emacs-directory))
-
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
@@ -23,6 +21,7 @@
     projectile
     helm-projectile
     company
+    zenburn-theme
     magit))
 
 (if (eq system-type 'darwin)
@@ -38,17 +37,30 @@
 (tool-bar-mode -1)
 (projectile-global-mode)
 (show-paren-mode 1)
-(load-theme 'dracula t)
+(load-theme 'zenburn t)
 (global-company-mode)
+(toggle-frame-fullscreen)
+
 
 ;; Clojure
+
+(require 'clj-refactor)
+
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (with-eval-after-load 'clojure-mode
   (add-hook 'clojure-mode-hook       #'enable-paredit-mode)
   (add-hook 'clojure-mode-hook       #'rainbow-delimiters-mode)
-  (add-hook 'clojure-mode-hook       #'cider-enlighten-mode)
+  (add-hook 'clojure-mode-hook       (lambda ()
+				       (clj-refactor-mode 1)
+				       (yas-minor-mode 1)
+				       (local-set-key (kbd "C-c C-r") #'cljr-helm)))
+
   (setq cider-debug-prompt 'minibuffer)
+  (setq cider-refresh-before-fn "dev/stop")
+
   (global-set-key (kbd "M-TAB") #'company-complete) ; use M-TAB, a.k.a. C-M-i, as manual trigger
   (define-key evil-normal-state-map (kbd "M-.") nil) ; override evil undo
-  (global-unset-key (kbd "M-."))) ; override etags
+  (local-unset-key (kbd "M-.")) ; override etags
 
+  (setq cljr-magic-requires :prompt)
+  (cljr-add-keybindings-with-prefix "\C-c r")) 
--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
-{:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
+{:user {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
         :injections   [(require 'pjstadig.humane-test-output)
                        (pjstadig.humane-test-output/activate!)]}
- :repl {:plugins      [[cider/cider-nrepl "0.11.0-snapshot"]
-                       [refactor-nrepl "2.0.0"]]
+ :repl {:plugins      [[refactor-nrepl "2.0.0"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
