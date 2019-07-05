6f8c737602f805c53e5776b1bba4bcf76977228d
Luis Mayorga <luismayorga@users.noreply.github.com>
2019-07-05 14:11:38 +0100

    Delete profiles.clj

--- .lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
-;        :injections [(require 'pjstadig.humane-test-output)
-                     ;(pjstadig.humane-test-output/activate!)]
-        }
- :test {:plugins       [[venantius/ultra "0.5.0"]]}
- :repl {:dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
